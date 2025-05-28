d23d71bff1d0e29c4e58e5d376b61f38b9ef4b6e
QuinnTsai <quinntsai@altek.com.tw>
2025-05-28 09:53:09 +0800

    Create the UartCtrl c# WPF solution

---
+++ PumpUtil/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PumpUtil
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
